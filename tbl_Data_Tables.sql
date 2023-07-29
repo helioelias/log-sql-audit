cdcf5240529e73365135373e0b533d398fb96690
Helio Elias <helioelias@gmail.com>
2023-07-28 21:08:51 -0300

    Add active for table

--- tbl_Data_Tables.sql
+++ tbl_Data_Tables.sql
@@ -1,8 +1,12 @@
+--Tabela que controla as tabela de auditoria
+
 CREATE TABLE [dbo].[tbl_Data_Tables] (
   [Id] [bigint] IDENTITY (1, 1) NOT NULL, 
   [Table_Name] [nvarchar] (100) NOT NULL, 
   [Log_Table_Name] [nvarchar] (104) NOT NULL, 
-  CONSTRAINT [PK_tbl_Data_Tables] PRIMARY KEY CLUSTERED ([Id]) WITH FILLFACTOR = 85 ON [PRIMARY]
+  [Active] [bit]  NOT NULL DEFAULT 1, 
+  [CreationDate] [datetime]  NOT NULL DEFAULT GETDATE(),
+  CONSTRAINT [PK_DataAuditTable] PRIMARY KEY CLUSTERED ([Id]) WITH FILLFACTOR = 85 ON [PRIMARY]
 ) ON [PRIMARY] 
 
 GO
